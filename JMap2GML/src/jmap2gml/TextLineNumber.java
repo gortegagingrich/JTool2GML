5f1ab00546b9fa3b7c3d339a452127c69b7e62e6
gortegagingrich <gortegagingrich@gmail.com>
2016-05-03 14:43:00 -0700

    added a source for the TextLineNumber class

--- JMap2GML/src/jmap2gml/TextLineNumber.java
+++ JMap2GML/src/jmap2gml/TextLineNumber.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Created by Rob Camick
+ * from https://tips4java.wordpress.com/2009/05/23/text-component-line-number/
  */
 package jmap2gml;
 
